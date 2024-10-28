11cb54a21f2897f27a3d07896ad2824d6837d783
Daniele Bogo <me@bogodaniele.com>
2024-10-28 15:47:21 +0100

    Revert previous change

--- podcasts/PlaybackEffects.swift
+++ podcasts/PlaybackEffects.swift
@@ -55,7 +55,7 @@ class PlaybackEffects {
         effects.isGlobal = false
 
         if FeatureFlag.customPlaybackSettings.enabled &&
-            shouldReflectGlobalSettings(for: podcast) {
+            !podcast.usedCustomEffectsBefore {
             let globalEffect = globalEffects()
             effects.trimSilence = globalEffect.trimSilence
             effects.volumeBoost = globalEffect.volumeBoost
@@ -132,15 +132,5 @@ class PlaybackEffects {
         }
 
         return value > 0 ? .low : .off
-    }
-
-    private class func shouldReflectGlobalSettings(for podcast: Podcast) -> Bool {
-        let globalEffect = globalEffects()
-        let trimSilence = podcast.settings.trimSilence.amount
-        let volumeBoost = podcast.settings.boostVolume
-        let playbackSpeed = podcast.settings.playbackSpeed
-        return globalEffect.trimSilence == trimSilence &&
-        globalEffect.volumeBoost == volumeBoost &&
-        globalEffect.playbackSpeed == playbackSpeed
     }
 }
