84f8f2f3063cc0900c4da8707f42814666cdc794
Brandon Titus <b@titus.io>
2024-10-29 17:11:40 -0600

    Fix layout alignment for podcast marquee

--- podcasts/End of Year/Stories/2024/NumberListened2024.swift
+++ podcasts/End of Year/Stories/2024/NumberListened2024.swift
@@ -15,59 +15,49 @@ struct NumberListened2024: ShareableStory {
     @State var bottomRowXOffset: Double = 0
 
     private let backgroundColor = Color(hex: "#EFECAD")
+    let identifier: String = "number_of_podcasts_and_episodes_listened"
 
     var body: some View {
-        VStack {
-            GeometryReader { geometry in
-                PodcastCoverContainer(geometry: geometry) {
-                    VStack(spacing: -20) {
-                        HStack(spacing: 16) {
-                            Group {
-                                let shadow = true
-                                podcastCover(0, shadow: shadow)
-                                podcastCover(1, shadow: shadow)
-                                podcastCover(2, shadow: shadow)
-                                podcastCover(3, shadow: shadow)
-                                podcastCover(0, shadow: shadow)
-                                podcastCover(1, shadow: shadow)
-                                podcastCover(2, shadow: shadow)
-                                podcastCover(3, shadow: shadow)
-                            }
-                            .frame(width: geometry.size.width * 0.4, height: geometry.size.width * 0.4)
-                        }
-                        .offset(x: topRowXOffset)
-                        .modifier(animationViewModel.animate($topRowXOffset, to: -300))
-
-                        HStack(spacing: 16) {
-                            Group {
-                                let shadow = false
-                                podcastCover(4, shadow: shadow)
-                                podcastCover(5, shadow: shadow)
-                                podcastCover(6, shadow: shadow)
-                                podcastCover(7, shadow: shadow)
-                                podcastCover(4, shadow: shadow)
-                                podcastCover(5, shadow: shadow)
-                                podcastCover(6, shadow: shadow)
-                                podcastCover(7, shadow: shadow)
-                            }
-                            .frame(width: geometry.size.width * 0.4, height: geometry.size.width * 0.4)
-                        }
-                        .padding(.leading, geometry.size.width * 0.35)
-                        .offset(x: bottomRowXOffset)
-                        .modifier(animationViewModel.animate($bottomRowXOffset, to: 300))
-                    }
-                    .rotationEffect(Angle(degrees: -15))
-                    .padding(.top, geometry.size.height * 0.1)
-                }
-                .onAppear {
-                    if animated {
-                        animationViewModel.play()
-                    }
-                }
-            }
+        VStack(alignment: .leading) {
+            Spacer()
+            podcastMarquees()
+            Spacer()
             footerView()
         }
         .background(backgroundColor)
+    }
+
+    @ViewBuilder func podcastMarquees() -> some View {
+        GeometryReader { geometry in
+            PodcastCoverContainer(geometry: geometry, alignment: .center) {
+                VStack(spacing: -28) {
+                    podcastMarquee(size: geometry.size, shadow: false, scale: 0.4, indices: [0, 1, 2, 3, 0, 1, 2, 3])
+                        .offset(x: topRowXOffset)
+                        .modifier(animationViewModel.animate($topRowXOffset, to: -300))
+                    podcastMarquee(size: geometry.size, shadow: true, scale: 0.5, indices: [4, 5, 6, 7, 4, 5, 6, 7])
+                        .padding(.leading, geometry.size.width * 0.35)
+                        .offset(x: bottomRowXOffset)
+                        .modifier(animationViewModel.animate($bottomRowXOffset, to: 300))
+                }
+                .rotationEffect(Angle(degrees: -15))
+            }
+            .onAppear {
+                if animated {
+                    animationViewModel.play()
+                }
+            }
+        }
+    }
+
+    @ViewBuilder func podcastMarquee(size: CGSize, shadow: Bool, scale: Double, indices: [Int]) -> some View {
+        HStack(spacing: 16) {
+            Group {
+                ForEach(indices, id: \.self) { index in
+                    podcastCover(index, shadow: shadow)
+                }
+            }
+            .frame(width: size.width * scale, height: size.width * scale)
+        }
     }
 
     @ViewBuilder func footerView() -> some View {
@@ -78,6 +68,7 @@ struct NumberListened2024: ShareableStory {
                 .font(.system(size: 15, weight: .light))
         }
         .padding(.horizontal, 24)
+        .padding(.vertical, 6)
     }
 
     @ViewBuilder
@@ -92,5 +83,16 @@ struct NumberListened2024: ShareableStory {
                     $0
                 }
             }
+    }
+
+    func onAppear() {
+        Analytics.track(.endOfYearStoryShown, story: identifier)
+    }
+
+    func sharingAssets() -> [Any] {
+        [
+            StoryShareableProvider.new(AnyView(self)),
+            StoryShareableText(L10n.eoyStoryListenedToNumbersShareText(listenedNumbers.numberOfPodcasts, listenedNumbers.numberOfEpisodes))
+        ]
     }
 }
