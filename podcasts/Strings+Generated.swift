5a3d7970632ff3d6ab25aca6a0a30b02934864aa
Daniele Bogo <me@bogodaniele.com>
2024-12-05 14:57:18 +0000

    Add Toast message

--- podcasts/Strings+Generated.swift
+++ podcasts/Strings+Generated.swift
@@ -3476,6 +3476,8 @@ internal enum L10n {
   internal static var upNextShuffleAnnouncementText: String { return L10n.tr("Localizable", "up_next_shuffle_announcement_text") }
   /// Introducing Shuffle
   internal static var upNextShuffleAnnouncementTitle: String { return L10n.tr("Localizable", "up_next_shuffle_announcement_title") }
+  /// Shuffle on. The next episode will be selected randomly.
+  internal static var upNextShuffleToastMessage: String { return L10n.tr("Localizable", "up_next_shuffle_toast_message") }
   /// Upgrade Account
   internal static var upgradeAccount: String { return L10n.tr("Localizable", "upgrade_account") }
   /// Save 50%% off your first year
