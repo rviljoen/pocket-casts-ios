ccba68e0ad7d7f4a05525706ab2c87d94130bb98
rviljoen <25402814+rviljoen@users.noreply.github.com>
2025-05-15 11:52:53 +0200

    Uncommited changes

--- .auto-resolution/Modules/Utils/Sources/PocketCastsUtils/Extensions/Double+Extensions.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Double {
-
-    /// Checks whether the value is not NaN or Infinite
-    public var isNumeric: Bool {
-        !self.isNaN && !self.isInfinite
-    }
-}
--- .conflict-side-1/.buildkite/commands/shared_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-echo "--- :ruby: Setting up Ruby tools"
-install_gems
-
-echo "--- :swift: Installing Swift Package Manager Dependencies"
-install_swiftpm_dependencies
